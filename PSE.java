6765040e4152eddc6b5f439b264d63c2617d05d0
SKai <skai@localhost.localdomain>
2017-11-08 19:09:48 +0000

    Interface atualizada e pronta para inserir algoritmos. Algos da Debora já inseridos

--- PSE.java
+++ PSE.java
@@ -4,11 +4,21 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import java.util.concurrent.TimeUnit;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.SwingWorker;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 
@@ -23,23 +33,26 @@ public class PSE extends JFrame {
     private final int timelineY = windowY * 1/13;
     private final int timelineButton1Y = timelineY;
     private final int timelineButton2Y = timelineY;
-    private final int buttonX = windowX * 1/10;
+    private final int buttonX = windowX * 1/8;
     private final int buttonY = windowY - timelineY;
     private final int gridX = windowX - buttonX;
     private final int gridY = windowY - timelineY;
-    private final String f1 = "Func1";
-    private final String f2 = "Func2";
-    private final String f3 = "Func3";
-    private final String f4 = "Func4";
-    private final String f5 = "Func5";
-    private final String f6 = "Func6";
+    private final String f1 = "Escala Cinza";
+    private final String f2 = "Negativo";
+    private final String f3 = "Media";
+    private final String f4 = "Gaussiano";
+    private final String f5 = "Laplaciano";
+    private final String f6 = "Sobel";
     private final String f7 = "Func7";
     private final String f8 = "Func8";
     private final String f9 = "Func9";
     private final String f10 = "Func10";
     private final String f11 = "Func11";
     private final String f12 = "Func12";
-    JPanel timelinePanel;
+    private JPanel timelinePanel;
+    private JPanel drawPanel;
+    private BufferedImage mainImage;
+    private JLabel mainImageLabel;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -78,6 +91,7 @@ public class PSE extends JFrame {
         JButton openButton = new JButton("Abrir");
         openButton.addActionListener((ActionEvent event) -> {
             setTitle("PSE - Abrir");
+            openImage();
         });
         timelineButtonPanel1.add(openButton);
         // Process
@@ -106,6 +120,7 @@ public class PSE extends JFrame {
         JButton saveButton = new JButton("Guardar");
         saveButton.addActionListener((ActionEvent event) -> {
             setTitle("PSE - Guardar");
+            saveImage();
         });
         timelineButtonPanel2.add(saveButton);
         // Exit Image
@@ -210,10 +225,43 @@ public class PSE extends JFrame {
 
         // Draw Panel
         // -------------------------------------------------------------------------
-        JPanel drawPanel = new JPanel();
+        drawPanel = new JPanel();
         drawPanel.setPreferredSize(new Dimension(gridX, gridY));
         drawPanel.setBackground(Color.LIGHT_GRAY);
         add(drawPanel);
+    }
+    
+    private void openImage() {
+        JFileChooser imageChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Imagens PNG ou JPG", "png", "jpg");
+        imageChooser.setFileFilter(filter);
+        int returnVal = imageChooser.showOpenDialog(drawPanel);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: "
+                    + imageChooser.getSelectedFile().getName());
+            try {
+                mainImage = ImageIO.read(imageChooser.getSelectedFile());
+                showImage();
+            } catch (IOException e) {
+            }
+        }
+    }
+    
+    private void showImage() {
+        if (mainImage != null) {
+            if (mainImageLabel != null) {
+                drawPanel.remove(mainImageLabel);
+            }
+            mainImageLabel = new JLabel(new ImageIcon(mainImage));
+            drawPanel.add(mainImageLabel);
+            drawPanel.repaint();
+            drawPanel.validate();
+        }
+    }
+
+    private void saveImage() {
+        
     }
 
     private void addTimeline(String func) {
@@ -254,6 +302,8 @@ public class PSE extends JFrame {
                     TimeUnit.MILLISECONDS.sleep(200);
                 } catch (Exception e) {
                 }
+                functionChooser(defaultText);
+                showImage();
                 button.setText(defaultText);
                 button.setBackground(defaultColor);
                 button.validate();
@@ -263,5 +313,300 @@ public class PSE extends JFrame {
             setTitle("PSE - Processamento - 100% - Completo");
             return 0;
         }
+        
+        private boolean functionChooser(String name) {
+                 if (name.equals(f1)) {
+                mainImage = EscalaDeCinza(mainImage);
+            }
+            else if (name.equals(f2)) {
+                mainImage = Negativo(mainImage);
+            }
+            else if (name.equals(f3)) {
+                mainImage = Media(mainImage);
+            }
+            else if (name.equals(f4)) {
+                mainImage = Gaussiano(mainImage);
+            }
+            else if (name.equals(f5)) {
+                mainImage = Laplaciano(mainImage);
+            }
+            else if (name.equals(f6)) {
+                mainImage = Sobel(mainImage);
+            }
+            return true;
+        }
+    }
+    
+// ------------------------------------- INSERIR ALGORITMOS ABAIXO --------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+    
+    public static BufferedImage EscalaDeCinza(BufferedImage imagem) {
+        //Imagem resultante
+        BufferedImage ResultImage = new BufferedImage (imagem.getColorModel(),imagem.copyData(null),imagem.getColorModel().isAlphaPremultiplied(),null);
+        
+        //pegar coluna e linha da imagem
+        int coluna = imagem.getWidth();
+        int linha = imagem.getHeight();
+
+        int media = 0;
+        //laço para varrer a matriz de pixels da imagem
+        for (int i = 0; i < coluna; i++) {
+            for (int j = 0; j < linha; j++) {               
+                //rgb recebe o valor RGB do pixel em questão                
+                int rgb = imagem.getRGB(i, j);              
+                int r = (int)((rgb&0x00FF0000)>>>16); //R
+                int g = (int)((rgb&0x0000FF00)>>>8);  //G
+                int b = (int) (rgb&0x000000FF);       //B
+
+                //media dos valores do RGB
+                //será o valor do pixel na nova imagem
+                media = (r + g + b) / 3;
+
+                //criar uma instância de Color
+                Color color = new Color(media, media, media);
+                //setar o valor do pixel com a nova cor
+                ResultImage.setRGB(i, j, color.getRGB());
+            }
+        }
+        return ResultImage;
+    }
+    
+    public static BufferedImage Negativo(BufferedImage imagem) {
+        //Imagem resultante
+        BufferedImage ResultImage = new BufferedImage (imagem.getColorModel(),imagem.copyData(null),imagem.getColorModel().isAlphaPremultiplied(),null);
+        
+        //pegar coluna e linha da imagem
+        int coluna = imagem.getWidth();
+        int linha = imagem.getHeight();
+        //laço para varrer a matriz de pixels da imagem
+        for (int i = 0; i < coluna; i++) {
+            for (int j = 0; j < linha; j++) {  
+                //rgb recebe o valor RGB do pixel em questão 
+                int rgb = imagem.getRGB(i, j);               
+                //a cor inversa é dado por 255 menos o valor da cor                 
+                int r = 255 - (int)((rgb&0x00FF0000)>>>16);
+                int g = 255 - (int)((rgb&0x0000FF00)>>>8);
+                int b = 255 - (int) (rgb&0x000000FF);
+                Color color = new Color(r, g, b);
+                ResultImage.setRGB(i, j, color.getRGB());
+            }
+        }
+        return ResultImage;
+    }
+    
+    public static BufferedImage Media (BufferedImage imagem) {
+        //imagem resultante
+        BufferedImage ResultImage = new BufferedImage (imagem.getColorModel(),imagem.copyData(null),imagem.getColorModel().isAlphaPremultiplied(),null);
+        
+        //mascara de média
+        int [][]mascaraMedia = {{1,1,1},
+                                {1,1,1},
+                                {1,1,1}};
+        //soma dos valores da máscara
+        int valorMascara = 9;
+        
+        //cores primarias
+        int r = 0, g = 0, b = 0;
+        
+        //pegar coluna e linha da imagem
+        int coluna = imagem.getWidth();
+        int linha = imagem.getHeight();
+        
+        //percorre a imagem
+        for (int i = 1; i + 1 < linha; i++) {
+            for (int j = 1; j + 1 < coluna; j++) {
+                //percorre a máscara
+                for (int l = -1; l <= 1; l++) {
+                    for (int k = -1; k <= 1; k++) {
+                        //rgb = rgb do pixel
+                        int rgb = imagem.getRGB(j + k, i + l);
+                        //pegando os valores das cores primarias de cada pixel após a convolucao com a máscara
+                        r += (mascaraMedia[1 + l][1 + k] * (int)((rgb&0x00FF0000)>>>16));
+                        g += (mascaraMedia[1 + l][1 + k] * (int)((rgb&0x0000FF00)>>>8));
+                        b += (mascaraMedia[1 + l][1 + k] * (int)((rgb&0x000000FF)));
+                    }
+                }
+                //dividia as cores pelo valor da máscara
+                r = r / valorMascara;
+                g = g / valorMascara;
+                b = b / valorMascara;
+                //nova cor do pixel
+                Color tempColor = new Color(r, g, b);
+                //setar o respectivel pixel na nova imagem
+                ResultImage.setRGB(j, i, tempColor.getRGB());
+                //zerar valor das cores primarias
+                r = g = b = 0;
+            }
+        }
+        ResultImage.getSubimage(1, 1, coluna-1, linha-1);
+        return ResultImage;
+    }
+    
+    public static BufferedImage Gaussiano (BufferedImage imagem) {
+        //imagem resultante
+        BufferedImage ResultImage = new BufferedImage (imagem.getColorModel(),imagem.copyData(null),imagem.getColorModel().isAlphaPremultiplied(),null);
+        //mascara
+        int [][]mascaraGaussiano = {{1,2,1},
+                                {2,4,2},
+                                {1,2,1}};
+        int valorMascara = 16;
+
+        int r = 0, g = 0, b = 0;
+        //tamanho imagem
+        int coluna = imagem.getWidth();
+        int linha = imagem.getHeight();
+        //percorre imagem
+        for (int i = 1; i + 1 < linha; i++) {
+            for (int j = 1; j + 1 < coluna; j++) {
+                //percorre mascara
+                for (int l = -1; l <= 1; l++) {
+                    for (int k = -1; k <= 1; k++) {
+                        //rgb
+                        int rgb = imagem.getRGB(j + k, i + l);
+                        //pegando os valores das cores primarias de cada pixel após a convolucao com a máscara
+                        r += (mascaraGaussiano[1 + l][1 + k] * (int)((rgb&0x00FF0000)>>>16));
+                        g += (mascaraGaussiano[1 + l][1 + k] * (int)((rgb&0x0000FF00)>>>8));
+                        b += (mascaraGaussiano[1 + l][1 + k] * (int)((rgb&0x000000FF)));
+                    }
+
+                }
+                //dividindo as cores pelo valor da máscara
+                r = r / valorMascara;
+                g = g / valorMascara;
+                b = b / valorMascara;
+                Color tempColor = new Color(r, g, b);
+                //setar novo valor do pixel na imagem resultante
+                ResultImage.setRGB(j, i, tempColor.getRGB());
+                r = g = b = 0;
+            }
+        }
+        ResultImage.getSubimage(1, 1, coluna-1, linha-1);
+        return ResultImage;
+    }
+
+    public static BufferedImage Laplaciano (BufferedImage imagem) {
+        //imagem resultante
+        BufferedImage ResultImage = new BufferedImage (imagem.getColorModel(),imagem.copyData(null),imagem.getColorModel().isAlphaPremultiplied(),null);
+        //mascaras
+        int [][]mascaraL1 = {{0,-1,0},
+                            {-1,4,-1},
+                            {0,-1,0}};
+        
+        int [][]mascaraL2 = {{1,1,1},
+                            {1,-8,1},
+                            {1,1,1}};
+
+        int r = 0, g = 0, b = 0;
+        //tamanho imagem
+        int coluna = imagem.getWidth();
+        int linha = imagem.getHeight();
+        //percorre imagem
+        for (int i = 1; i + 1 < linha; i++) {
+            for (int j = 1; j + 1 < coluna; j++) {
+                //percorre mascara
+                for (int l = -1; l <= 1; l++) {
+                    for (int k = -1; k <= 1; k++) {
+                        //rgb
+                        int rgb = imagem.getRGB(j + k, i + l);
+                        //pegando os valores das cores primarias de cada pixel após a convolucao com a máscara
+                        r += (mascaraL1[1 + l][1 + k] * (int)((rgb&0x00FF0000)>>>16));
+                        g += (mascaraL1[1 + l][1 + k] * (int)((rgb&0x0000FF00)>>>8));
+                        b += (mascaraL1[1 + l][1 + k] * (int)((rgb&0x000000FF)));
+                    }
+
+                }
+                //arredondamento de valores
+                Color tempColor = new Color(Math.min(255, Math.max(0, r)), Math.min(255, Math.max(0, g)), Math.min(255, Math.max(0, b)));
+                ResultImage.setRGB(j, i, tempColor.getRGB());
+                r = g = b = 0;
+            }
+        }
+        //percorre imagem
+        for (int i = 1; i + 1 < imagem.getHeight(); i++) {
+            for (int j = 1; j + 1 < imagem.getWidth(); j++) {
+                //percorre mascara
+                for (int l = -1; l <= 1; l++) {
+                    for (int k = -1; k <= 1; k++) {
+                        //rgb
+                        int rgb = imagem.getRGB(j + k, i + l);
+                        //pegando os valores das cores primarias de cada pixel após a convolucao com a máscara
+                        r += (mascaraL2[1 + l][1 + k] * (int)((rgb&0x00FF0000)>>>16));
+                        g += (mascaraL2[1 + l][1 + k] * (int)((rgb&0x0000FF00)>>>8));
+                        b += (mascaraL2[1 + l][1 + k] * (int)((rgb&0x000000FF)));
+                    }
+
+                }
+                //arredondamento de valores
+                Color tempColor = new Color(Math.min(255, Math.max(0, r)), Math.min(255, Math.max(0, g)), Math.min(255, Math.max(0, b)));
+                ResultImage.setRGB(j, i, tempColor.getRGB());
+                r = g = b = 0;
+            }
+        }
+        ResultImage.getSubimage(1, 1, coluna-1, linha-1);
+        return ResultImage;
+    }
+    
+    public static BufferedImage Sobel (BufferedImage imagem) {
+        //imagem resultante
+        BufferedImage ResultImage = new BufferedImage (imagem.getColorModel(),imagem.copyData(null),imagem.getColorModel().isAlphaPremultiplied(),null);
+        //mascaras
+        int [][]mascaraS1 = {{-1,-2,-1},
+                             {0,0,0},
+                             {1,2,1}};
+        int [][]mascaraS2 = {{-1,0,1},
+                             {-2,0,2},
+                             {-1,0,1}};
+
+        int r = 0, g = 0, b = 0;
+        //tamanho da imagem
+        int coluna = imagem.getWidth();
+        int linha = imagem.getHeight();
+        
+        //percorre imagem
+        for (int i = 1; i + 1 < linha; i++) {
+            for (int j = 1; j + 1 < coluna; j++) {
+                //percorre mascara
+                for (int l = -1; l <= 1; l++) {
+                    for (int k = -1; k <= 1; k++) {
+                        //rgb
+                        int rgb = imagem.getRGB(j + k, i + l);
+                        //pegando os valores das cores primarias de cada pixel após a convolucao com a máscara
+                        r += (mascaraS1[1 + l][1 + k] * (int)((rgb&0x00FF0000)>>>16));
+                        g += (mascaraS1[1 + l][1 + k] * (int)((rgb&0x0000FF00)>>>8));
+                        b += (mascaraS1[1 + l][1 + k] * (int)((rgb&0x000000FF)));
+                    }
+
+                }
+                //arredondamento de valores
+                Color tempColor = new Color(Math.min(255, Math.max(0, r)), Math.min(255, Math.max(0, g)), Math.min(255, Math.max(0, b)));
+                ResultImage.setRGB(j, i, tempColor.getRGB());
+                r = g = b = 0;
+            }
+        }
+        //percorrer imagem
+        for (int i = 1; i + 1 < imagem.getHeight(); i++) {
+            for (int j = 1; j + 1 < imagem.getWidth(); j++) {
+                //Percorrer máscara
+                for (int l = -1; l <= 1; l++) {
+                    for (int k = -1; k <= 1; k++) {
+                        //RGB
+                        int rgb = imagem.getRGB(j + k, i + l);
+                        //pegando os valores das cores primarias de cada pixel após a convolucao com a máscara
+                        r += (mascaraS2[1 + l][1 + k] * (int)((rgb&0x00FF0000)>>>16));
+                        g += (mascaraS2[1 + l][1 + k] * (int)((rgb&0x0000FF00)>>>8));
+                        b += (mascaraS2[1 + l][1 + k] * (int)((rgb&0x000000FF)));
+                    }
+
+                }
+                //Arredondamento dos valores
+                Color tempColor = new Color(Math.min(255, Math.max(0, r)), Math.min(255, Math.max(0, g)), Math.min(255, Math.max(0, b)));
+                ResultImage.setRGB(j, i, tempColor.getRGB());
+                r = g = b = 0;
+            }
+        }
+        ResultImage.getSubimage(1, 1, coluna-1, linha-1);
+        return ResultImage;
     }
 }
